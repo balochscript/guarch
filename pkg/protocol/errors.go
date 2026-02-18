2ef7a7dc0581c861b7e462bb95bcf36eb159b012
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2026-02-19 03:11:31 +0330

    Add error definitions for protocol handling

---
+++ pkg/protocol/errors.go
@@ -0,0 +1,16 @@
+package protocol
+
+import "errors"
+
+var (
+	ErrPacketTooShort    = errors.New("guarch: packet too short")
+	ErrPacketTooLarge    = errors.New("guarch: packet exceeds max size")
+	ErrInvalidVersion    = errors.New("guarch: invalid protocol version")
+	ErrInvalidPacketType = errors.New("guarch: invalid packet type")
+
+	ErrAuthFailed  = errors.New("guarch: authentication failed")
+	ErrAuthTimeout = errors.New("guarch: authentication timeout")
+
+	ErrDecryptFailed    = errors.New("guarch: decryption failed")
+	ErrConnectionClosed = errors.New("guarch: connection closed")
+)
