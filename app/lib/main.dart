ef8e2abd314f2945883f870335d1d33820c8e412
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2026-02-20 22:54:50 +0330

    Create main.dart

---
+++ app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:guarch/app.dart';
+import 'package:guarch/providers/app_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  final appProvider = AppProvider();
+  await appProvider.init();
+
+  runApp(
+    ChangeNotifierProvider.value(
+      value: appProvider,
+      child: const GuarchApp(),
+    ),
+  );
+}
