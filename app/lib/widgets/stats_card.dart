26930ae75a567afbbab33c587ad7276d28afe5f9
Salim Balòc Kòhgradi <soltansalim97@gmail.com>
2026-02-21 01:08:05 +0330

    Update StatsCard colors to use kGold

--- app/lib/widgets/stats_card.dart
+++ app/lib/widgets/stats_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:guarch/models/connection_state.dart';
+import 'package:guarch/app.dart';
 
 class StatsCard extends StatelessWidget {
   final ConnectionStats stats;
@@ -19,6 +20,7 @@ class StatsCard extends StatelessWidget {
                 fontSize: 32,
                 fontWeight: FontWeight.w300,
                 letterSpacing: 4,
+                color: kGold,
               ),
             ),
             const SizedBox(height: 16),
@@ -27,16 +29,15 @@ class StatsCard extends StatelessWidget {
                 Expanded(
                   child: _buildStat(
                     icon: Icons.arrow_upward,
-                    color: Colors.orange,
+                    color: kGold,
                     speed: stats.uploadSpeedText,
                     total: stats.totalUploadText,
-                    label: 'Upload',
                   ),
                 ),
                 Container(
                   width: 1,
                   height: 50,
-                  color: Colors.grey.withOpacity(0.3),
+                  color: kGold.withOpacity(0.2),
                 ),
                 Expanded(
                   child: _buildStat(
@@ -44,7 +45,6 @@ class StatsCard extends StatelessWidget {
                     color: Colors.green,
                     speed: stats.downloadSpeedText,
                     total: stats.totalDownloadText,
-                    label: 'Download',
                   ),
                 ),
               ],
@@ -60,7 +60,6 @@ class StatsCard extends StatelessWidget {
     required Color color,
     required String speed,
     required String total,
-    required String label,
   }) {
     return Column(
       children: [
@@ -82,7 +81,7 @@ class StatsCard extends StatelessWidget {
         const SizedBox(height: 4),
         Text(
           total,
-          style: const TextStyle(color: Colors.grey, fontSize: 11),
+          style: TextStyle(color: kGold.withOpacity(0.4), fontSize: 11),
         ),
       ],
     );
