90f942cae0e8c3e52d93d4e520def92875eb654f
Baloch Script <soltansalim97@gmail.com>
2026-05-21 11:04:47 +0330

    Update interface.go

--- pkg/transport/interface.go
+++ pkg/transport/interface.go
@@ -1,27 +1,28 @@
 package transport
 
 import (
-	"context"
-	"net"
+    "context"
+    "net"
+    "time"
 )
 
-type Transport interface {
-	Dial(ctx context.Context) (net.Conn, error)
-	Name() string
-	Close() error
+type Config struct {
+    Type             string
+    Host             string
+    Port             int
+    Path             string
+    UseTLS           bool
+    Headers          map[string]string
+    FallbackOrder    []string
+    DialTimeout      time.Duration
+    HandshakeTimeout time.Duration
 }
 
-type Config struct {
-	Type   string
-	Host   string
-	Port   int
-	Path   string
-	UseTLS bool
-	Headers map[string]string
-	DialTimeout    int
-	HandshakeTimeout int
+type Transport interface {
+    Dial(ctx context.Context) (net.Conn, error)
+    Name() string
 }
 
 type Factory interface {
-	Create(cfg *Config) (Transport, error)
+    Create(cfg *Config) (Transport, error)
 }
