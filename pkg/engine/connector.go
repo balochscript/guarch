24b6289ec35dd53cc56dcce680327047ac89cf8e
Baloch Script <soltansalim97@gmail.com>
2026-05-22 09:36:41 +0330

    Update connector.go

--- pkg/engine/connector.go
+++ pkg/engine/connector.go
@@ -146,7 +146,7 @@ func (c *Connector) getTransportConfig() *transport.Config {
             Type:             "direct",
             Host:             host,
             Port:             port,
-            UseTLS:           false,
+            UseTLS:           true,
             FallbackOrder:    []string{},
             DialTimeout:      30 * time.Second,
             HandshakeTimeout: 15 * time.Second,
