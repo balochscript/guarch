1ae3c3556ab976786d644baaebb8bf3588abe8d5
Baloch Script <soltansalim97@gmail.com>
2026-05-24 15:39:58 +0330

    Format code in interface.go for consistency

--- pkg/transport/interface.go
+++ pkg/transport/interface.go
@@ -1,28 +1,29 @@
 package transport
 
 import (
-    "context"
-    "net"
-    "time"
+	"context"
+	"net"
+	"time"
 )
 
 type Config struct {
-    Type             string
-    Host             string
-    Port             int
-    Path             string
-    UseTLS           bool
-    Headers          map[string]string
-    FallbackOrder    []string
-    DialTimeout      time.Duration
-    HandshakeTimeout time.Duration
+	Type             string
+	Host             string
+	Port             int
+	Path             string
+	UseTLS           bool
+	Headers          map[string]string
+	FallbackOrder    []string
+	DialTimeout      time.Duration
+	HandshakeTimeout time.Duration
+	DNSServers       []string
 }
 
 type Transport interface {
-    Dial(ctx context.Context) (net.Conn, error)
-    Name() string
+	Dial(ctx context.Context) (net.Conn, error)
+	Name() string
 }
 
 type Factory interface {
-    Create(cfg *Config) (Transport, error)
+	Create(cfg *Config) (Transport, error)
 }
