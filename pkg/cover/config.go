18cd793a5650f076dae6254e72693d47dfd43261
Baloch Script <soltansalim97@gmail.com>
2026-07-11 21:46:58 +0330

    Update config.go

--- pkg/cover/config.go
+++ pkg/cover/config.go
@@ -25,6 +25,10 @@ type ModeConfig struct {
 	MaxPadding       int
 	BatteryThreshold int
 	HysteresisDelay  time.Duration
+	IdleThreshold    int64
+	LightThreshold   int64
+	MediumThreshold  int64
+	HeavyThreshold   int64
 }
 
 func NewConfig() *Config {
@@ -40,11 +44,9 @@ func (c *Config) Validate() error {
 	if !c.Enabled {
 		return nil
 	}
-	
 	if len(c.Domains) == 0 {
 		return fmt.Errorf("cover: no domains configured")
 	}
-	
 	for i, d := range c.Domains {
 		if d.Domain == "" {
 			return fmt.Errorf("cover: domain %d is empty", i)
@@ -56,24 +58,52 @@ func (c *Config) Validate() error {
 			return fmt.Errorf("cover: domain %d has negative weight", i)
 		}
 	}
-	
 	return nil
 }
 
 func (c *Config) ApplyDefaults() {
-	if c.MaxConcurrent == 0 {
+	if c.MaxConcurrent <= 0 {
 		c.MaxConcurrent = 3
 	}
-	
+	if c.MaxConcurrent > 10 {
+		c.MaxConcurrent = 10
+	}
 	for i := range c.Domains {
-		if c.Domains[i].Weight == 0 {
+		if c.Domains[i].Weight <= 0 {
 			c.Domains[i].Weight = 10
 		}
-		if c.Domains[i].MinInterval == 0 {
+		if c.Domains[i].MinInterval <= 0 {
 			c.Domains[i].MinInterval = 2 * time.Second
 		}
-		if c.Domains[i].MaxInterval == 0 {
+		if c.Domains[i].MaxInterval <= 0 {
 			c.Domains[i].MaxInterval = 10 * time.Second
 		}
+		if c.Domains[i].MaxInterval < c.Domains[i].MinInterval {
+			c.Domains[i].MaxInterval = c.Domains[i].MinInterval + 5*time.Second
+		}
+	}
+}
+
+func ParseBytesPerMin(s string) int64 {
+	if s == "" {
+		return 0
+	}
+	var val int64
+	var unit string
+	_, err := fmt.Sscanf(s, "%d%s", &val, &unit)
+	if err != nil {
+		return 0
+	}
+	switch unit {
+	case "B/min", "B", "b/min", "":
+		return val
+	case "KB/min", "KB", "K", "kB/min":
+		return val * 1000
+	case "MB/min", "MB", "M", "mB/min":
+		return val * 1000 * 1000
+	case "GB/min", "GB", "G":
+		return val * 1000 * 1000 * 1000
+	default:
+		return val * 1000
 	}
 }
